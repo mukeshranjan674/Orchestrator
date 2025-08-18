b9cc1c56bf64bc583409d5c5fee7eab25719a52b
mukeshranjan674 <mukeshranjan674@gmail.com>
2025-08-18 21:37:00 +0530

    added run.sh file

---
+++ src/main/resources/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+APP_NAME="orchestrator"
+JAR_PATH="target/Orchestrator-1.jar"
+LOG_FILE="app.log"
+
+echo ">>> Pulling latest code..."
+git pull origin main
+
+echo ">>> Building project..."
+mvn clean package -DskipTests
+
+echo ">>> Stopping old process..."
+PID=$(pgrep -f $JAR_PATH)
+if [ -n "$PID" ]; then
+  echo ">>> Killing PID $PID"
+  kill -9 $PID
+else
+  echo ">>> No process running."
+fi
+
+echo ">>> Starting new JAR..."
+nohup java -jar $JAR_PATH > $LOG_FILE 2>&1 &
+
+NEW_PID=$!
+echo ">>> New app started with PID $NEW_PID"
