75a8c8f6d22a41a52da27361b194963b5d86a615
mukeshranjan674 <mukeshranjan674@gmail.com>
2025-08-18 22:00:23 +0530

    added new api

--- src/main/java/com/example/orchestrator/controllers/TestController.java
+++ src/main/java/com/example/orchestrator/controllers/TestController.java
@@ -1,5 +1,7 @@
 package com.example.orchestrator.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +13,11 @@ public class TestController {
 	@GetMapping("/test")
 	public String test() {
 		return "Testing Successful";
+	}
+	
+	@GetMapping("/test2")
+	public List<String> test2(){
+		return List.of("Testing 1", "Testing 2");
 	}
 
 }
